fd4f5d5f2024885d45d0ae08b4c73cefc842640c
Inderjot Singh <inderjotsingh141@gmail.com>
2024-03-16 00:50:20 +0530

    leetcode code run logic almost done

---
+++ backend/src/example/test.js
@@ -0,0 +1,68 @@
+import fs from 'fs'
+
+class Main {
+    constructor(testCases) {
+        this.testCases = testCases;
+    }
+
+    reverseArray(array) {
+        return array.reverse();
+    }
+
+    validation() {
+        const totalTestCases = this.testCases.length;
+        let testPassed = 0;
+        let hasErrorOccurred = false;
+        let error = { input: '', expected: '', error: '' };
+
+        this.testCases.forEach((testCase) => {
+            try {
+                const response = this.reverseArray(testCase.input);
+                if (this.isArraySame(response, testCase.output)) {
+                    testPassed++;
+                }
+            } catch (err) {
+                if (!hasErrorOccurred) {
+                    error.input = testCase.input;
+                    error.expected = testCase.output;
+                    error.error = err;
+                    hasErrorOccurred = true;
+                }
+            }
+        });
+
+        return {
+            totalTestCases: this.testCases.length,
+            noOfTestCasesPassed: testPassed,
+            success: this.testCases.length === testPassed,
+            error: error
+        };
+    }
+
+    isArraySame(first, second) {
+        try {
+            for (let i = 0; i < first.length; i++) {
+                if (first[i] !== second[i]) {
+                    return false;
+                }
+            }
+            return true;
+        } catch {
+            return false;
+        }
+    }
+}
+
+
+function main() {
+
+    const data = fs.readFileSync('/tmp/input', 'utf8');
+    const testCases = JSON.parse(data)
+    const main = new Main(testCases);
+    const response = JSON.stringify(main.validation());
+    fs.writeFileSync('/tmp/result', response);
+
+}
+
+
+main()
--- backend/src/lambda.mjs
+++ backend/src/lambda.mjs
@@ -2,27 +2,42 @@ import fs from 'fs'
 import child_process from 'child_process'
 import path from 'path'
 
+
+
+
 export const handler = async (event) => {
+
+    // todo add more validation
     const code = event.code;
     const lang = event.lang;
+
+    // strinfied testcases
+    const testCases = event.testCases
+
+
+    // test cases in file
+    // test cases will be read by code
+    fs.writeFileSync('/tmp/input', testCases)
+
+
+
     let result, error, isError;
-    const tempDir = '/tmp';
+    const TEMP_DIR = '/tmp';
+    const RESULT_FILE = '/tmp/result'
+    let stateTime = new Date().getTime()
 
     try {
-        const outputFile = path.join(tempDir, `Main`);
+        const outputFile = path.join(TEMP_DIR, `Main`);
 
         switch (lang.toLowerCase()) {
             case 'javascript':
                 execNode(code, outputFile);
-                result = fs.readFileSync(outputFile, 'utf8');
                 break;
             case 'python':
                 execPython(code, outputFile);
-                result = fs.readFileSync(outputFile, 'utf8');
                 break;
             case 'java':
                 execJava(code, outputFile);
-                result = fs.readFileSync(outputFile, 'utf8');
                 break;
             case 'cpp':
                 result = execCpp(code, outputFile);
@@ -34,18 +49,21 @@ export const handler = async (event) => {
 
         }
 
-        fs.unlinkSync(outputFile);
         isError = false;
+        result = fs.readFileSync(RESULT_FILE, 'utf8');
 
     } catch (err) {
         error = err.toString();
         isError = true;
     }
 
+    let endTime = new Date().getTime()
+
     const response = {
         result,
         error,
         isError,
+        time: endTime - stateTime
     }
 
     return {
@@ -59,34 +77,28 @@ export const handler = async (event) => {
 function execNode(code, outputFile) {
     const tempFile = `${outputFile}.js`
     fs.writeFileSync(tempFile, code)
-    child_process.execSync(`node ${tempFile} > ${outputFile}`)
-    fs.unlinkSync(tempFile)
+    child_process.execSync(`node ${tempFile}`)
 }
 
 function execPython(code, outputFile) {
     const tempFile = `${outputFile}.py`;
     fs.writeFileSync(tempFile, code);
-    child_process.execSync(`python3 ${tempFile} > ${outputFile}`);
-    fs.unlinkSync(tempFile);
+    child_process.execSync(`python3 ${tempFile}`);
 }
 
 function execJava(code, outputFile) {
     const tempFile = `${outputFile}.java`;
     fs.writeFileSync(tempFile, code);
     child_process.execSync(`javac ${tempFile}`);
-    child_process.execSync(`java -cp ${path.dirname(tempFile)} ${path.basename(tempFile, '.java')} > ${outputFile}`);
-    fs.unlinkSync(tempFile);
-    fs.unlinkSync(`${tempFile.replace('.java', '.class')}`);
+    child_process.execSync(`java -cp ${path.dirname(tempFile)} ${path.basename(tempFile, '.java')}`);
 }
 
 function execCpp(code, outputFile) {
     const tempFile = `${outputFile}.cpp`;
     fs.writeFileSync(tempFile, code);
     child_process.execSync(`g++ ${tempFile} -o ${outputFile}`);
-    child_process.execSync(`${outputFile} > ${outputFile}.out`);
+    child_process.execSync(`${outputFile}`);
     const result = fs.readFileSync(`${outputFile}.out`, 'utf8');
-    fs.unlinkSync(tempFile);
-    fs.unlinkSync(`${outputFile}.out`);
     return result;
 }
 
